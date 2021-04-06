e07a3535f18084121ed167376e508bf404ce41af
oneday <youshihou5@gmail.com>
2021-04-06 22:02:17 +0800

    update code

--- code/14_interview_block___block/14_interview_block___block/main.m
+++ code/14_interview_block___block/14_interview_block___block/main.m
@@ -2,10 +2,18 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void(^Block)(void);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        __block int age = 10;
+        __block NSObject *object = [[NSObject alloc] init];
+        Block block = ^{
+            age = 20;
+            object = nil;
+            NSLog(@"%d", age);
+        };
+        block();
     }
     return 0;
 }

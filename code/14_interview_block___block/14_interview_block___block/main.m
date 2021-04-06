ef8986b13621d7864135b90651c264ef3aa8f6ec
oneday <youshihou5@gmail.com>
2021-04-06 22:02:10 +0800

    update code

---
+++ code/14_interview_block___block/14_interview_block___block/main.m
@@ -0,0 +1,11 @@
+//___FILEHEADER___
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

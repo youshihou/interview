063a608e2a591d52a9312edb504baed201b95c10
oneday <youshihou5@gmail.com>
2021-04-06 18:01:49 +0800

    update code

--- code/13_interview_block_auto_object/13_interview_block_auto_object/ViewController.m
+++ code/13_interview_block_auto_object/13_interview_block_auto_object/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import "Person.h"
 
 @interface ViewController ()
 
@@ -15,8 +16,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+//    Person *p = [[Person alloc] init];
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        NSLog(@"%@", p);
+//    });
+//    NSLog(@"%s", __func__);
+    
+//    Person *p = [[Person alloc] init];
+//    __weak Person *weakP = p;
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        NSLog(@"%@", weakP);
+//    });
+//    NSLog(@"%s", __func__);
+
+    
+//    Person *p = [[Person alloc] init];
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//            NSLog(@"%@", p);
+//        });
+//    });
+//    NSLog(@"%s", __func__);
+
+    
+//    Person *p = [[Person alloc] init];
+//    __weak Person *weakP = p;
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//            NSLog(@"%@", weakP);
+//        });
+//    });
+//    NSLog(@"%s", __func__);
+    
+    
+//    Person *p = [[Person alloc] init];
+//    __weak Person *weakP = p;
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        NSLog(@"1------%@", weakP);
+//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//            NSLog(@"2------%@", p);
+//        });
+//    });
+//    NSLog(@"%s", __func__);
+
+    
+    Person *p = [[Person alloc] init];
+    __weak Person *weakP = p;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSLog(@"1------%@", p);
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSLog(@"2------%@", weakP);
+        });
+    });
+    NSLog(@"%s", __func__);
+
+}
 
 @end

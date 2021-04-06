39ad34041ee3f472c4e2cccc2f95dc8dc3caf70f
oneday <youshihou5@gmail.com>
2021-04-06 09:52:04 +0800

    update code

---
+++ code/11_interview_block/11_interview_block/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  11_interview_block
+//
+//  Created by Ankui on 4/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

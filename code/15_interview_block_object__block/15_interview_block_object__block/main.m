fbe58ec69a3bcc5fd7e0d0093d2382b384f530d7
oneday <youshihou5@gmail.com>
2021-04-07 10:16:04 +0800

    update code

--- code/15_interview_block_object__block/15_interview_block_object__block/main.m
+++ code/15_interview_block_object__block/15_interview_block_object__block/main.m
@@ -6,11 +6,44 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Person.h"
+
+typedef void(^Block)(void);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+//        __block Person *p = [[Person alloc] init];
+        
+//        Person *p = [[Person alloc] init];
+//        __block __weak Person *weakP = p;
+//        Block block = ^{
+////            NSLog(@"%p", p);
+//            NSLog(@"%p", weakP);
+//        };
+//        block();
+        
+        
+//        Block block;
+//        {
+//            __block Person *p = [[Person alloc] init];
+//            block = ^{
+//                NSLog(@"%p", p);
+//            };
+//        }
+//        block();
+        
+        
+        Block block;
+        {
+            Person *p = [[Person alloc] init];
+            __block __weak Person *weakP = p;
+            block = ^{
+                NSLog(@"%p", weakP);
+            };
+        }
+        block();
+
     }
     return 0;
 }

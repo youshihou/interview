c37cfce946c2211ae4c1f4e31397fb638a3291d4
oneday <youshihou5@gmail.com>
2021-04-07 10:15:51 +0800

    update code

---
+++ code/15_interview_block_object__block/15_interview_block_object__block/Person.m
@@ -0,0 +1,12 @@
+//
+//  Person.m
+//  15_interview_block_object__block
+//
+//  Created by Ankui on 4/7/21.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
---
+++ code/15_interview_block_object__block/15_interview_block_object__block/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  15_interview_block_object__block
+//
+//  Created by Ankui on 4/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

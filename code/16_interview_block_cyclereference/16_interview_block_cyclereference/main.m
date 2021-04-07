b3b115f1f203b65f92e0ae35b4c4a6387d8cca38
oneday <youshihou5@gmail.com>
2021-04-07 20:14:37 +0800

    update code

--- code/16_interview_block_cyclereference/16_interview_block_cyclereference/main.m
+++ code/16_interview_block_cyclereference/16_interview_block_cyclereference/main.m
@@ -10,17 +10,26 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        Person *p = [[Person alloc] init];
+//        Person *p = [[Person alloc] init];
+//        p.age = 10;
+////        __weak Person *weakP = p;
+////        __weak typeof(p) weakP = p;
+////        __unsafe_unretained typeof(p) weakP = p;
+////        p.block = ^{
+////            NSLog(@"%d", weakP.age);
+////        };
+//
+//        [p test];
+        
+        
+        __block Person *p = [[Person alloc] init];
         p.age = 10;
-//        __weak Person *weakP = p;
-//        __weak typeof(p) weakP = p;
-//        __unsafe_unretained typeof(p) weakP = p;
-//        p.block = ^{
-//            NSLog(@"%d", weakP.age);
-//        };
-        
-        [p test];
-        
+        p.block = ^{
+            NSLog(@"%d", p.age);
+            p = nil;
+        };
+        p.block();
+
         NSLog(@"-------------");
     }
     return 0;

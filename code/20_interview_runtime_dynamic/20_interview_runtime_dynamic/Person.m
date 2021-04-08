2aa797dbad5f6514f0831150503ee9772ae0c069
oneday <youshihou5@gmail.com>
2021-04-08 21:38:41 +0800

    update code

---
+++ code/20_interview_runtime_dynamic/20_interview_runtime_dynamic/Person.m
@@ -0,0 +1,12 @@
+//
+//  Person.m
+//  20_interview_runtime_dynamic
+//
+//  Created by Ankui on 4/8/21.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
---
+++ code/20_interview_runtime_dynamic/20_interview_runtime_dynamic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  20_interview_runtime_dynamic
+//
+//  Created by Ankui on 4/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

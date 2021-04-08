968e4f0e5c7b3f6b94c6e2e2f0c1b2b62c0054b3
oneday <youshihou5@gmail.com>
2021-04-08 16:50:19 +0800

    update code

--- code/18_interview_runtime_resolver/18_interview_runtime_resolver/Person.m
+++ code/18_interview_runtime_resolver/18_interview_runtime_resolver/Person.m
@@ -14,6 +14,11 @@
 //    NSLog(@"%s", __func__);
 //}
 
+
+void c_other(id self, SEL _cmd) {
+    NSLog(@"%s, %@, %@", __func__, self, NSStringFromSelector(_cmd));
+}
+
 - (void)other {
     NSLog(@"%s", __func__);
 }
@@ -21,10 +26,13 @@
 + (BOOL)resolveInstanceMethod:(SEL)sel {
 //    NSLog(@"%s", __func__);
     if (@selector(test) == sel) {
-        Method method = class_getInstanceMethod(self, @selector(other));
-        IMP imp = method_getImplementation(method);
-        const char *types = method_getTypeEncoding(method);
-        class_addMethod(self, sel, imp, types);
+//        Method method = class_getInstanceMethod(self, @selector(other));
+//        IMP imp = method_getImplementation(method);
+//        const char *types = method_getTypeEncoding(method);
+//        class_addMethod(self, sel, imp, types);
+        
+        class_addMethod(self, sel, (IMP)c_other, "v16@0:8");
+        
         return YES;
     }
     return [super resolveInstanceMethod:sel];

ad4fd5cb4ed9b2249883153b90b9b5127ad25d1e
oneday <youshihou5@gmail.com>
2021-04-08 16:26:41 +0800

    update code

--- code/18_interview_runtime_resolver/18_interview_runtime_resolver/Person.m
+++ code/18_interview_runtime_resolver/18_interview_runtime_resolver/Person.m
@@ -6,7 +6,28 @@
 //
 
 #import "Person.h"
+#import <objc/runtime.h>
 
 @implementation Person
+
+//- (void)test {
+//    NSLog(@"%s", __func__);
+//}
+
+- (void)other {
+    NSLog(@"%s", __func__);
+}
+
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+//    NSLog(@"%s", __func__);
+    if (@selector(test) == sel) {
+        Method method = class_getInstanceMethod(self, @selector(other));
+        IMP imp = method_getImplementation(method);
+        const char *types = method_getTypeEncoding(method);
+        class_addMethod(self, sel, imp, types);
+        return YES;
+    }
+    return [super resolveInstanceMethod:sel];
+}
 
 @end
--- code/18_interview_runtime_resolver/18_interview_runtime_resolver/main.m
+++ code/18_interview_runtime_resolver/18_interview_runtime_resolver/main.m
@@ -6,11 +6,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Person.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Person *p = [[Person alloc] init];
+        [p test];
     }
     return 0;
 }

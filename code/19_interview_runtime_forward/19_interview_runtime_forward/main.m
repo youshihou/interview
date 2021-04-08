eb1c41e9934cc8ad7231b8c718a38fbe0cb4f947
oneday <youshihou5@gmail.com>
2021-04-08 17:17:27 +0800

    update code

---
+++ code/19_interview_runtime_forward/19_interview_runtime_forward/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  19_interview_runtime_forward
+//
+//  Created by Ankui on 4/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

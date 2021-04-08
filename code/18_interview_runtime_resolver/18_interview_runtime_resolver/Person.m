0d5305740f37ca921ea97ae9b09877e42c2b05fc
oneday <youshihou5@gmail.com>
2021-04-08 16:26:27 +0800

    update code

---
+++ code/18_interview_runtime_resolver/18_interview_runtime_resolver/Person.m
@@ -0,0 +1,12 @@
+//
+//  Person.m
+//  18_interview_runtime_resolver
+//
+//  Created by Ankui on 4/8/21.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
---
+++ code/18_interview_runtime_resolver/18_interview_runtime_resolver/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  18_interview_runtime_resolver
+//
+//  Created by Ankui on 4/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

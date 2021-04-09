46f1459a776c4ee9713fa8333d47ac4738369a1f
oneday <youshihou5@gmail.com>
2021-04-09 12:28:38 +0800

    update code

---
+++ code/22_interview_runtime_class/22_interview_runtime_class/Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  22_interview_runtime_class
+//
+//  Created by Ankui on 4/9/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ code/22_interview_runtime_class/22_interview_runtime_class/Person.m
@@ -0,0 +1,12 @@
+//
+//  Person.m
+//  22_interview_runtime_class
+//
+//  Created by Ankui on 4/9/21.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end

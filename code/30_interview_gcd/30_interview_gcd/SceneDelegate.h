cac1b6812f70fd74d1e3b963ea917e95bbafe425
oneday <youshihou5@gmail.com>
2021-04-11 12:28:17 +0800

    update code

---
+++ code/30_interview_gcd/30_interview_gcd/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  30_interview_gcd
+//
+//  Created by Ankui on 4/10/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ code/30_interview_gcd/30_interview_gcd/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  30_interview_gcd
+//
+//  Created by Ankui on 4/10/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

fcab35e27583d5483bff522d35f28aedf4913ff0
oneday <youshihou5@gmail.com>
2021-04-11 21:04:34 +0800

    update code

---
+++ code/32_interview_lock/32_interview_lock/Lock/MutexTask.h
@@ -0,0 +1,16 @@
+//
+//  MutexTask.h
+//  32_interview_lock
+//
+//  Created by Ankui on 4/11/21.
+//
+
+#import "BaseTask.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MutexTask : BaseTask
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ code/32_interview_lock/32_interview_lock/Lock/MutexTask.m
@@ -0,0 +1,12 @@
+//
+//  MutexTask.m
+//  32_interview_lock
+//
+//  Created by Ankui on 4/11/21.
+//
+
+#import "MutexTask.h"
+
+@implementation MutexTask
+
+@end

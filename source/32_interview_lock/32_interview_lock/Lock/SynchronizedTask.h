be610fa27ec8e83b34dc635d32b5dcb397e5c1c1
oneday <youshihou5@gmail.com>
2021-04-12 11:44:23 +0800

    update code

---
+++ source/32_interview_lock/32_interview_lock/Lock/SynchronizedTask.h
@@ -0,0 +1,16 @@
+//
+//  SynchronizedTask.h
+//  32_interview_lock
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import "BaseTask.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SynchronizedTask : BaseTask
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ source/32_interview_lock/32_interview_lock/Lock/SynchronizedTask.m
@@ -0,0 +1,12 @@
+//
+//  SynchronizedTask.m
+//  32_interview_lock
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import "SynchronizedTask.h"
+
+@implementation SynchronizedTask
+
+@end

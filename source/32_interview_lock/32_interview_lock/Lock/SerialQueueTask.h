4e892f2c56b8a77ac0c31b648a5a7f7cdfc3ac1b
oneday <youshihou5@gmail.com>
2021-04-12 10:50:50 +0800

    update code

---
+++ source/32_interview_lock/32_interview_lock/Lock/SerialQueueTask.h
@@ -0,0 +1,16 @@
+//
+//  SerialQueueTask.h
+//  32_interview_lock
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import "BaseTask.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SerialQueueTask : BaseTask
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ source/32_interview_lock/32_interview_lock/Lock/SerialQueueTask.m
@@ -0,0 +1,12 @@
+//
+//  SerialQueueTask.m
+//  32_interview_lock
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import "SerialQueueTask.h"
+
+@implementation SerialQueueTask
+
+@end

2e13c006a448f2058b83beda9082eed9103ed8a7
oneday <youshihou5@gmail.com>
2021-04-12 15:33:32 +0800

    update code

---
+++ source/33_interview_readwrite/33_interview_readwrite/ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  33_interview_readwrite
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import "ViewController.h"
+#import <pthread.h>
+
+@interface ViewController ()
+
+@property (nonatomic, assign) pthread_rwlock_t lock;
+
+@end
+
+@implementation ViewController
+
+- (void)dealloc {
+    pthread_rwlock_destroy(&_lock);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    pthread_rwlock_init(&_lock, NULL);
+    
+    dispatch_queue_t queue = dispatch_queue_create(0, 0);
+    for (NSInteger i = 0; i < 10; i++) {
+        dispatch_async(queue, ^{
+            [self read];
+        });
+        dispatch_async(queue, ^{
+            [self write];
+        });
+    }
+}
+
+- (void)read {
+    pthread_rwlock_rdlock(&_lock);
+    sleep(1);
+    NSLog(@"%s", __func__);
+    pthread_rwlock_unlock(&_lock);
+}
+
+- (void)write {
+    pthread_rwlock_wrlock(&_lock);
+    sleep(1);
+    NSLog(@"%s", __func__);
+    pthread_rwlock_unlock(&_lock);
+}
+
+@end

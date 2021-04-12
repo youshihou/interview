8efa56d9bc701436d343a3c1b3e4c36d8c9af71a
oneday <youshihou5@gmail.com>
2021-04-12 19:07:42 +0800

    update code

--- source/35_interview_gcd_timer/35_interview_gcd_timer/ViewController.m
+++ source/35_interview_gcd_timer/35_interview_gcd_timer/ViewController.m
@@ -9,13 +9,25 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) dispatch_source_t timer;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    NSLog(@"%s", __func__);
+    
+    dispatch_queue_t queue = dispatch_get_main_queue();
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC, 1 * NSEC_PER_SEC);
+    dispatch_source_set_event_handler(timer, ^{
+        NSLog(@"666");
+    });
+    dispatch_resume(timer);
+    self.timer = timer;
 }
 
 

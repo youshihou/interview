b9ab41098dc08069a12d54602528901bfa574d42
oneday <youshihou5@gmail.com>
2021-04-12 18:05:16 +0800

    update code

---
+++ source/34_interview_timer/34_interview_timer/ProxyTask.h
@@ -0,0 +1,16 @@
+//
+//  ProxyTask.h
+//  34_interview_timer
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProxyTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ source/34_interview_timer/34_interview_timer/ProxyTask.m
@@ -0,0 +1,12 @@
+//
+//  ProxyTask.m
+//  34_interview_timer
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import "ProxyTask.h"
+
+@implementation ProxyTask
+
+@end

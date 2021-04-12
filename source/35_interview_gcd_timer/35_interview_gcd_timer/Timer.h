5c903c3b592538e02e29f6fefd77a4e08367cecd
oneday <youshihou5@gmail.com>
2021-04-12 19:53:20 +0800

    update code

---
+++ source/35_interview_gcd_timer/35_interview_gcd_timer/Timer.h
@@ -0,0 +1,16 @@
+//
+//  Timer.h
+//  35_interview_gcd_timer
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Timer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ source/35_interview_gcd_timer/35_interview_gcd_timer/Timer.m
@@ -0,0 +1,12 @@
+//
+//  Timer.m
+//  35_interview_gcd_timer
+//
+//  Created by Ankui on 4/12/21.
+//
+
+#import "Timer.h"
+
+@implementation Timer
+
+@end

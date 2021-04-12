74ec6610cc4f7a5ce609e7cf47c0cae8d18ac859
oneday <youshihou5@gmail.com>
2021-04-12 17:15:08 +0800

    update code

--- source/34_interview_timer/34_interview_timer/ViewController.m
+++ source/34_interview_timer/34_interview_timer/ViewController.m
@@ -9,13 +9,35 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) CADisplayLink *link;
+@property (nonatomic, strong) NSTimer *timer;
+
 @end
 
 @implementation ViewController
 
+- (void)dealloc {
+    NSLog(@"%s", __func__);
+//    [self.link invalidate];
+    [self.timer invalidate];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+//    self.link = [CADisplayLink displayLinkWithTarget:self selector:@selector(test)];
+//    [self.link addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    
+//    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(test) userInfo:nil repeats:YES];
+    
+    __weak typeof(self) weakSelf = self;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        [weakSelf test];
+    }];
+}
+
+- (void)test {
+    NSLog(@"%s", __func__);
 }
 
 

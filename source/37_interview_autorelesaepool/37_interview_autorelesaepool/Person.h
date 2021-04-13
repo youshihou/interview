2ddf8e41547e4ab25527ddab27c8aae76e976bd2
oneday <youshihou5@gmail.com>
2021-04-13 22:06:41 +0800

    update code

---
+++ source/37_interview_autorelesaepool/37_interview_autorelesaepool/Person.h
@@ -0,0 +1,16 @@
+//
+//  Person.h
+//  37_interview_autorelesaepool
+//
+//  Created by Ankui on 4/13/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ source/37_interview_autorelesaepool/37_interview_autorelesaepool/Person.m
@@ -0,0 +1,12 @@
+//
+//  Person.m
+//  37_interview_autorelesaepool
+//
+//  Created by Ankui on 4/13/21.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
